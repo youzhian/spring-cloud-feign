1a796bc9fbbc821caf7e1cbacc4ef692485a0360
youzhian <youzhian@aliyun.com>
2021-05-07 10:00:28 +0800

    修改gateway相关配置

--- feign-gateway/src/main/java/com/vly/feign/GateWayApplication.java
+++ feign-gateway/src/main/java/com/vly/feign/GateWayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
  * @author youzhian
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class GateWayApplication {
 
     public static void main(String []args){
